d6f9baea3230edf73b301dc75eea27719263b69a
Conor Bailey <cpbailey025@gmail.com>
2023-03-28 16:29:35 +0100

    Minor changes to homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,63 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// function([string1, string2],target id,[color1,color2])    
+consoleText([
+	'NumPy.', 'MatPlotLib.', 'TensorFlow.', 'Keras.', 'Pandas.', 
+	'PyTorch.', 'Plotly.', 'Seaborn.', 'SkLearn.', 'PIL.'], 
+	'text',
+	['#f28482', '#ee9b00', '#003049', '#81b29a', '#2f3e46', 
+	'#eb5e28', '#bc4749', '#495057', '#b56576', '#3d5a80']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
 function hideInfo() {
 	var x = document.getElementById("myHidden");
 	if (x.style.display === "none") {
